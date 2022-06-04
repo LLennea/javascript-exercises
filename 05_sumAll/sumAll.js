ea7e98519b3ee9d4212a239504c59ffe999876e0
Liam Lennea <liamlennea2002@gmail.com>
2022-06-04 17:24:04 -0600

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    if (numOne < 0 || numTwo < 0) {
+        return 'ERROR'
+    } else if (typeof numOne !== 'number' || typeof numTwo !== 'number') {
+        return 'ERROR'
+    }
+    let sum = 0;
+    if (numOne < numTwo) {
+        for (let i = numOne; i <= numTwo; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = numTwo; i <= numOne; i++) {
+            sum += i;
+        }
+    }
+    return sum
 };
 
 // Do not edit below this line
