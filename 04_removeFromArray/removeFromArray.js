b2a852a521b91ed362a745a57a464339d5aaad0e
Liam Lennea <liamlennea2002@gmail.com>
2022-06-04 17:15:06 -0600

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    newArray = array;
+    for (let i = 1; i <= arguments.length - 1; i++) {
+        let index = newArray.indexOf(arguments[i]);
+        if (index !== -1) {
+            newArray.splice(index, 1);
+        }
+    }
+    return newArray
 };
 
 // Do not edit below this line
