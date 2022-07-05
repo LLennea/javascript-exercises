c1b0f56e790453820d0d30cd0706443621d6efea
Liam Lennea <liamlennea2002@gmail.com>
2022-07-05 17:37:59 -0600

    Complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const currDate = new Date();
+    arr.sort((person1, person2) => {
+        if (!person1.yearOfDeath && !person2.yearOfDeath) {
+            return (currDate.getFullYear() - person1.yearOfBirth) - (currDate.getFullYear() - person2.yearOfBirth)
+        } else if (!person1.yearOfDeath) {
+            return (currDate.getFullYear() - person1.yearOfBirth) - (person2.yearOfDeath - person2.yearOfBirth)
+        } else if (!person2.yearOfDeath) {
+            return (person1.yearOfDeath - person1.yearOfBirth) - (currDate.getFullYear() - person2.yearOfBirth)
+        } else {
+            return (person1.yearOfDeath - person1.yearOfBirth) - (person2.yearOfDeath - person2.yearOfBirth)
+        }
+    });
+    return arr.pop()
 };
 
 // Do not edit below this line
