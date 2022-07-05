b62c75f94e7f0622c2924425c4e4b8a4fd2e09a3
Liam Lennea <liamlennea2002@gmail.com>
2022-07-05 16:58:03 -0600

    Complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = +num;
+    if (num < 1) {
+        return 'OOPS'
+    }
+    let currNum = 1;
+    let prevNum = 0;
+    let nextNum = 0;
+    while (num > 1) {
+        nextNum = currNum + prevNum;
+        prevNum = currNum;
+        currNum = nextNum;
+        num--;
+    };
+    return currNum
 };
 
 // Do not edit below this line
