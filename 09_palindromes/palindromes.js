d1da63a6f198b20f58afde5101ae93d368be376a
Liam Lennea <liamlennea2002@gmail.com>
2022-07-05 16:38:12 -0600

    Complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[^\w\s\']|_/g, "")
+             .replace(/\s+/g, "")
+             .toUpperCase();
+    return str === str.split("").reverse().join("")
 };
 
 // Do not edit below this line
